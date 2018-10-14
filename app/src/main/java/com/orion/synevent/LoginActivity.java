cea77199025d1675061f635d2ec12b04157fcfef
Francisco Reyes <servoclay@gmail.com>
2018-10-13 22:10:04 -0300

    Trabajando en la actividad login

--- app/src/main/java/com/orion/synevent/LoginActivity.java
+++ app/src/main/java/com/orion/synevent/LoginActivity.java
@@ -2,6 +2,18 @@ package com.orion.synevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class LoginActivity extends AppCompatActivity {
 
