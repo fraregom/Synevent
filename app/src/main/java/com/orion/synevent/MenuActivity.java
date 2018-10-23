e7b93b951f91309382da7198968a3cb21ed29259
Benjamin Gautier O <benjamin.gautier.14@sansano.usm.cl>
2018-10-23 01:23:15 -0300

    Date&Time Picker listo, falta formatear bien la elección en los textviews, presentable :D

--- app/src/main/java/com/orion/synevent/MenuActivity.java
+++ app/src/main/java/com/orion/synevent/MenuActivity.java
@@ -3,16 +3,15 @@ package com.orion.synevent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.widget.Toolbar;
 
 
 
