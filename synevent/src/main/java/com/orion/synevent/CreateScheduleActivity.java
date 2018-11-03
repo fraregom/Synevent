4d327b6416ee5889e01831ce35f5beddffa315a6
Benjamin Gautier O <benjamin.gautier.14@sansano.usm.cl>
2018-11-03 00:47:04 -0300

    ListView Con lista personalizada lista, falta generalizar el tema na más.

--- synevent/src/main/java/com/orion/synevent/CreateScheduleActivity.java
+++ synevent/src/main/java/com/orion/synevent/CreateScheduleActivity.java
@@ -1,9 +1,11 @@
 package com.orion.synevent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,8 @@ public class CreateScheduleActivity extends AppCompatActivity implements Adapter
 
     private Spinner spinner;
     private static final String[] paths = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado" , "Domingo"};
+    private Button mCreateSchedule;
+    private Button mCancelSchedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,28 @@ public class CreateScheduleActivity extends AppCompatActivity implements Adapter
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        mCancelSchedule = findViewById(R.id.btn_cancel_schedule_create);
+        mCreateSchedule = findViewById(R.id.btn_create_schedule);
+
+        mCancelSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        mCreateSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MenuActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 
