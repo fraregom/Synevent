cc942f6e01288376e6e82c4367534f0fe92de17d
Benjamin Gautier O <benjamin.gautier.14@sansano.usm.cl>
2018-11-05 15:05:39 -0300

    Nuevo tab en mis eventos

--- synevent/src/main/java/com/orion/synevent/ListEventActivity.java
+++ synevent/src/main/java/com/orion/synevent/ListEventActivity.java
@@ -2,13 +2,17 @@ package com.orion.synevent;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -26,6 +30,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -99,6 +104,7 @@ public class ListEventActivity extends AppCompatActivity implements TabHost.TabC
         tabHost.setup();
         tabHost.addTab(getTabSpec1(tabHost));
         tabHost.addTab(getTabSpec2(tabHost));
+        tabHost.addTab(getTabSpec3(tabHost));
     }
 
     public void initializeVars() {
@@ -118,16 +124,57 @@ public class ListEventActivity extends AppCompatActivity implements TabHost.TabC
                 .setContent(this);
     }
 
+    private TabHost.TabSpec getTabSpec3(TabHost tabHost) {
+        TabHost.TabSpec aa =  tabHost.newTabSpec("Third Tab")
+                .setIndicator("Join It")
+                .setContent(this);
+        //getJoinedEvents();
+        return aa;
+    }
+
     @Override
     public View createTabContent(String tag) {
         // Full ArrayAdapters to list the events
-
+        //getJoinedEvents();
         if(tag == "Second Tab")
         {
             return  LayoutInflater.from(this).inflate(R.layout.finished_events,null);
-        }
+        }else if(tag == "Third Tab"){
+            //LinearLayout ll = (LinearLayout)getLayoutInflater().inflate(R.layout.join_event, null, true);
+            View aa = LayoutInflater.from(this).inflate(R.layout.join_event,null);
+            //setContentView(aa);
 
-        return LayoutInflater.from(this).inflate(R.layout.actives_events, null);
+            //getJoinedEvents();
+            return  aa;
+        }else{
+            return LayoutInflater.from(this).inflate(R.layout.actives_events, null);
+        }
+    }
+
+    private void getJoinedEvents() {
+        ArrayList<HashMap<String,String>> list_events_joined = new ArrayList<>();
+        HashMap<String, String> ayu = new HashMap<>();
+        ayu.put("text1", "Ayudantía Sistemas Operativos");
+        ayu.put("number_of_users_in_event", "78 participantes");
+        ayu.put("end_event", "finaliza el 98");
+        list_events_joined.add(ayu);
+
+
+        HashMap<String, String> ayu2 = new HashMap<>();
+        ayu2.put("text1", "Ayudantía Sistemas Distribuidos");
+        ayu2.put("number_of_users_in_event", "18 participantes");
+        ayu2.put("end_event", "finaliza el 18");
+        list_events.add(ayu2);
+
+        ListAdapter adapter;
+
+        adapter = new SimpleAdapter(this, list_events, R.layout.item_event_frame,
+                new String[]{"text1", "number_of_users_in_event", "end_event"},
+                new int[]{R.id.text1, R.id.number_of_users_in_event, R.id.end_event});
+
+
+        ListView lv2 = findViewById(R.id.lv_joined_events);
+        lv2.setAdapter(adapter);
     }
 
     public void setToolbar(){
@@ -137,5 +184,11 @@ public class ListEventActivity extends AppCompatActivity implements TabHost.TabC
         mToolbar.setTitleMargin(200, 2, 15, 2);
         mToolbar.setTitleTextColor(Color.WHITE);
         DrawerUtil.getDrawer(this, mToolbar);
+    }
+
+    public void joinToEvent(View view) {
+        // connect to server to suscribe to event
+        EditText et_code = findViewById(R.id.code_join_event);
+        Toast.makeText(this,et_code.getText().toString(), Toast.LENGTH_LONG).show();
     }
 }
